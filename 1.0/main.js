2f25145684104272e6999ecfc8abfc306054c438
hongfei <941116197@qq.com>
2018-09-28 09:32:00 +0800

    for循环

---
+++ 1.0/main.js
@@ -0,0 +1,24 @@
+//alert('good')
+//var r = confirm('你怎么了');
+//console.log("r:",r);
+//var name = prompt('输入你的名字')
+//console.log("name:",name);
+//以上为window对象下confirm和prompt属性；
+// window.setTimeout(function() {
+// console.log('3s到了');
+    
+// }, 300);
+// console.log('yo')
+// console.log('nice')
+// var count=0;
+//  var timer=setInterval(function(){
+// count++;
+// if(count>5){
+//     clearInterval(timer);
+//     return;
+// }
+
+// console.log('yo'+count);
+// },1000)
+
+
